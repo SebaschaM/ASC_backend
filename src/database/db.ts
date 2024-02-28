f015e87373f9bd9c8eddb0f868bd9db54e5fb981
Sebastián Chaquila <schaquila@autonoma.edu.pe>
2024-02-27 23:29:11 -0500

    Refactor database connection to use connection pooling

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,39 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: "postgres",
+  host: "34.30.8.232",
+  database: "asc_db",
+  password: "postsebas123",
+  port: 5432,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to database via pool");
+});
+
+// Realiza una verificación inicial de la conexión al arrancar la aplicación.
+const verifyConnection = async () => {
+  try {
+    const client = await pool.connect(); // Intenta obtener una conexión del pool
+    await client.query('SELECT 1'); // Realiza una consulta simple
+    client.release(); // Libera la conexión
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+    pool.end(); // Considera si realmente necesitas terminar el pool aquí.
+  }
+};
+
+// Llama a la función de verificación inmediatamente.
+verifyConnection();
+
+pool.on("error", (err) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
 async function dbConnect() {
-  const client = new Client({
-    user: 'postgres',
-    host: '34.30.8.232',
-    database: 'asc_db',
-    password: 'postsebas123',
-    port: 5432,
-  });
-
-    try {
-    await client.connect();
-    console.log("Connected to database");
-    return client;
-  } catch (e) {
-    console.log("Error connecting to database");
-    console.log(e);
-  }
+  return pool;
 }
 
 export default dbConnect;
